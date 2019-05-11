1ba093ee2ae095f2613ee30a8118cf04ccecd402
Roman Struna <roman.struna.dev@gmail.com>
2019-05-11 12:12:22 +0200

    added loaded file after you add the javascript code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function(){
   jQuery('<div/>', {
-    id: '_123456',
+    id: 'nr_integrated_cb',
+    "data-pid": '123456',
     class: 'fx-cb-div'
   }).appendTo('body');
+  $('#nr_integrated_cb').load("new.html");
 });
