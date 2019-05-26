865ee43354cff6b8079139892152c9ce95b1bc8f
Roman Struna <roman.struna.dev@gmail.com>
2019-05-26 19:14:00 +0200

    I created a function that calls the creation of an above the fold element that is loaded from an external html source. I will make into a chatbot, it requires an ID to work, that will later be checked on the backend aswell

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-jQuery(document).ready(function(){
-  jQuery('<div/>', {
-    id: 'nr_integrated_cb',
-    "data-pid": '123456',
-    class: 'fx-cb-div'
-  }).appendTo('body');
-  $('#nr_integrated_cb').load("new.html");
-});
+var frodx_chatBot = function (object) {
+  var resultObject = {};
+  resultObject.id = "nr_integrated_cb";
+  resultObject.class = "fx-cb-div";
+  if (!object.pid || typeof(object.pid) !== 'number' || object.pid.toString().length !== 6) {
+    return;
+  } else {
+    resultObject["data-pid"] = object.pid;
+  }
+  console.log(object.name, this);
+  $('<div/>', resultObject).appendTo('body');
+  $("#nr_integrated_cb").load("new.html");
+};
+
+frodx_chatBot({
+  pid: 457896,
+  backgroundColor: "red",
+  autoOpen: true
+})
