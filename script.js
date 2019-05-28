58b9ad1b64aa3c83cc96999680c7bb9b6ab8f00a
Roman Struna <roman.struna.dev@gmail.com>
2019-05-28 23:33:33 +0200

    added hex regex test for color and auto open check

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ var frodx_chatBot = function (object) {
   var resultObject = {};
   resultObject.id = "nr_integrated_cb";
   resultObject.class = "fx-cb-div";
+  var hexPattern = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
   if (!object.pid || typeof(object.pid) !== 'number' || object.pid.toString().length !== 6) {
     return;
   } else {
     resultObject["data-pid"] = object.pid;
+  }
+  if (!hexPattern.test(object.backgroundColor)) {
+    console.log('error with background color')
+  } else {
+    resultObject["backgroundColor"] = object.backgroundColor;
+  }
+  if (object.autoOpen === true) {
+    resultObject["auto-open"] = true;
+  } else {
+    resultObject["auto-open"] = false;
+    console.log('error with auto open')
   }
   console.log(object.name, this);
   $('<div/>', resultObject).appendTo('body');
@@ -14,7 +26,7 @@ var frodx_chatBot = function (object) {
 
 frodx_chatBot({
   pid: 457896,
-  backgroundColor: "red",
+  backgroundColor: "#f0f",
   autoOpen: true,
   borderColor: "#fff",
   borderWidth: 2,
